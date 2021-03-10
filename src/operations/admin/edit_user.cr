badcb76cf37aea25773ab38a809bc3dccd813c35
Nolan Mayersky <n.mayersky@hey.com>
2021-03-10 17:55:11 -0600

    Restored

--- src/operations/admin/edit_user.cr
+++ src/operations/admin/edit_user.cr
@@ -28,6 +28,6 @@ class Admin::EditUser < User::SaveOperation
   end
 
   private def delete_old_profile_picture(image_id)
-    Shrine::UploadedFile.new(id: image_id).delete
+    Shrine::UploadedFile.new(id: image_id, storage_key: "store").delete
   end
 end
