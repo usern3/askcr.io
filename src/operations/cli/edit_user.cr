ccc1a731fb1ea3825a17f8a6b98689e5cea96304
nolyoi <n.mayersky@hey.com>
2021-03-20 22:26:21 -0500

    Fix CLI

--- src/operations/cli/edit_user.cr
+++ src/operations/cli/edit_user.cr
@@ -2,19 +2,19 @@ class CLI::EditUser < User::SaveOperation
   needs user : User
   permit_columns email, role,
 
-  after_save log_changes
+  # after_save log_changes
 
-  def log_changes(user : User)
-    # Get changed attributes and log each of them
-    attributes.select(&.changed?).each do |attribute|
-      Log.dexter.info do
-        {
-          user_id:           user.id,
-          changed_attribute: attribute.name.to_s,
-          from:              attribute.original_value.to_s,
-          to:                attribute.value.to_s,
-        }
-      end
-    end
-  end
+  # def log_changes(user : User)
+  #   # Get changed attributes and log each of them
+  #   attributes.select(&.changed?).each do |attribute|
+  #     Log.dexter.info do
+  #       {
+  #         user_id:           user.id,
+  #         changed_attribute: attribute.name.to_s,
+  #         from:              attribute.original_value.to_s,
+  #         to:                attribute.value.to_s,
+  #       }
+  #     end
+  #   end
+  # end
 end
