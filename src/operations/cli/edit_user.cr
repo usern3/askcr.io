0265f430e7fd4cf344755bd76bc3baeacd1da277
nolyoi <n.mayersky@hey.com>
2021-03-20 22:15:27 -0500

    Added op for cli

---
+++ src/operations/cli/edit_user.cr
@@ -0,0 +1,20 @@
+class CLI::EditUser < User::SaveOperation
+  needs user : User
+  permit_columns email, role,
+
+  after_save log_changes
+
+  def log_changes(user : User)
+    # Get changed attributes and log each of them
+    attributes.select(&.changed?).each do |attribute|
+      Log.dexter.info do
+        {
+          user_id:           user.id,
+          changed_attribute: attribute.name.to_s,
+          from:              attribute.original_value.to_s,
+          to:                attribute.value.to_s,
+        }
+      end
+    end
+  end
+end
--- tasks/change_user_role.cr
+++ tasks/change_user_role.cr
@@ -29,7 +29,7 @@ class ChangeUserRole < LuckyCli::Task
     if disable?
       previous_role = user.role
       if user.role.value > 0
-        EditUser.update!(user, role: User::Role.new(:member))
+        CLI::EditUser.update!(user, role: User::Role.new(:member))
         puts "User #{email} is no longer an admin.".colorize(:green)
       else
         puts "User #{email} is not an admin.".colorize(:red)
@@ -38,7 +38,7 @@ class ChangeUserRole < LuckyCli::Task
       if user.role == new_role
         puts "User #{email} is already a(n) #{role.downcase}.".colorize(:red)
       else
-        EditUser.update!(user, role: new_role)
+        CLI::EditUser.update!(user, role: new_role)
         puts "User #{email} is now a(n) #{role.downcase}.".colorize(:green)
       end
     end
