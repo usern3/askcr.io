f712f386e800b8281fe6bf3963cc4b79724b334c
nolyoi <n.mayersky@hey.com>
2021-03-22 17:04:44 -0500

    Attempt 7k

--- config/shrine.cr
+++ config/shrine.cr
@@ -6,7 +6,6 @@ Shrine.configure do |config|
   else
     client = Awscr::S3::Client.new(ENV["BUCKETEER_AWS_REGION"], ENV["BUCKETEER_AWS_ACCESS_KEY_ID"], ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"])
     storage = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"] , client: client, prefix: "public", public: true)
-    config.storages["cache"] = Storage::FileSystem.new("uploads", prefix: "cache")
     config.storages["store"] = storage
   end
 end
