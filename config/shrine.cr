5245b7c0d15baff529e61ef4fe69494aa0254c7d
nolyoi <n.mayersky@hey.com>
2021-03-22 16:51:43 -0500

    Shrine attempt 2000

--- config/shrine.cr
+++ config/shrine.cr
@@ -1,36 +1,14 @@
 Shrine.configure do |config|
   if Lucky::Env.test?
-    config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::Memory.new
+    config.storages["cache"] = Storage::FileSystem.new("uploads", prefix: "cache")
+    config.storages["store"] = Storage::FileSystem.new("uploads")
   elsif Lucky::Env.test?
-    config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::FileSystem.new("tmp", prefix: "assets/uploads")
+    config.storages["cache"] = Storage::FileSystem.new("uploads", prefix: "cache")
+    config.storages["store"] = Storage::FileSystem.new("uploads")
   else
     client = Awscr::S3::Client.new(ENV["BUCKETEER_AWS_REGION"], ENV["BUCKETEER_AWS_ACCESS_KEY_ID"], ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"])
-    config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"] , client: client, prefix: "public/uploads", public: true)
-  end
-end
-
-# Defines the `Shrine` storage locations for different data types.
-module ShrineStorage
-  UPLOADS = "uploads"
-end
-
-# TRICKY: we patch this because it's not providing the correct download url
-class Shrine
-  module Storage
-    class S3 < Storage::Base
-      def url(id : String, **options) : String
-        presigned_options = Awscr::S3::Presigned::Url::Options.new(
-          aws_access_key: client.@aws_access_key,
-          aws_secret_key: client.@aws_secret_key,
-          region: client.@region,
-          object: object_key(id),
-          bucket: bucket,
-          host_name: endpoint
-        )
-
-        url = Awscr::S3::Presigned::Url.new(presigned_options)
-        url.for(:get)
-      end
-    end
+    storage = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"] , client: client, prefix: "public/uploads", public: true)
+    config.storages["cache"] = Storage::FileSystem.new("uploads", prefix: "cache")
+    config.storages["store"] = storage
   end
 end
