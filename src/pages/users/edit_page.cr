17e3266232b55e9e988d52ebc6d88359ee57403d
nolyoi <n.mayersky@hey.com>
2021-03-22 18:36:33 -0500

    Switch to 4xl

--- src/pages/users/edit_page.cr
+++ src/pages/users/edit_page.cr
@@ -5,7 +5,7 @@ class Users::EditPage < MainLayout
 
   def content
     div class: "container mx-auto mt-2 min-h-screen" do
-      div class: "w-full max-w-6xl mx-auto" do
+      div class: "w-full max-w-4xl mx-auto" do
         div class: "grid gap-2 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3" do
           div class: "sm:col-span-1 md:col-span-3 align-baseline" do
             h1 "Editing user: #{current_user.username}(#{current_user.email})", class: "text-2xl text-gray-800 font-bold align-baseline"
