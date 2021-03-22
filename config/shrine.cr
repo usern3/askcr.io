0c647e6505ca87c43a0148e9a01236793a7df0e0
nolyoi <n.mayersky@hey.com>
2021-03-22 17:37:09 -0500

    Final try for now.

--- config/shrine.cr
+++ config/shrine.cr
@@ -1,63 +1,16 @@
-require "shrine/plugins/determine_mime_type"
-
-client = Awscr::S3::Client.new(ENV["BUCKETEER_AWS_REGION"], ENV["BUCKETEER_AWS_ACCESS_KEY_ID"], ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"])
 Shrine.configure do |config|
-  config.storages["uploads"] = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"], client: client, prefix: "public/uploads", public: true)
-end
-
-class ShrineUploader < Shrine
-  load_plugin(
-    Shrine::Plugins::DetermineMimeType,
-    analyzer: Shrine::Plugins::DetermineMimeType::Tools::File
-  )
-
-  finalize_plugins!
-end
-
-
-module Lucky
-  class Storage
-
-    def self.upload(content : IO, filename : String, container : String)
-      # Shrine::UploadedFile
-      result = ShrineUploader.upload(content, container, metadata: { "filename" => filename })
-
-      # HACK: https://github.com/jetrockets/shrine.cr/issues/12
-      uri = URI.parse(result.url)
-      String.build do |str|
-        str << "https://"
-        str << uri.host.to_s << uri.path
-      end
-    end
-  end
-  endclient = Awscr::S3::Client.new(ENV["BUCKETEER_AWS_REGION"], ENV["BUCKETEER_AWS_ACCESS_KEY_ID"], ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"])
-  Shrine.configure do |config|
-    config.storages["uploads"] = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"], client: client, prefix: "public/uploads", public: true)
-  end
-
-  class ShrineUploader < Shrine
-    load_plugin(
-      Shrine::Plugins::DetermineMimeType,
-      analyzer: Shrine::Plugins::DetermineMimeType::Tools::File
-    )
-
-    finalize_plugins!
+  if Lucky::Env.test?
+    config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::Memory.new
+  elsif Lucky::Env.test?
+    config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::FileSystem.new("tmp", prefix: "assets/uploads")
+  else
+    client = Awscr::S3::Client.new(ENV["BUCKETEER_AWS_REGION"], ENV["BUCKETEER_AWS_ACCESS_KEY_ID"], ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"], endpoint: "https://bucketeer-7b46713e-bb3a-4b4f-a6f3-36fbd08c6baa.s3.amazonaws.com/public/")
+    storage = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"], client: client, prefix: "public", public: true)
+    config.storages["store"] = storage
   end
 end
-
-module Lucky
-  class Storage
-
-    def self.upload(content : IO, filename : String, container : String)
-      # Shrine::UploadedFile
-      result = ShrineUploader.upload(content, container, metadata: { "filename" => filename })
-
-      # HACK: https://github.com/jetrockets/shrine.cr/issues/12
-      uri = URI.parse(result.url)
-      String.build do |str|
-        str << "https://"
-        str << uri.host.to_s << uri.path
-      end
-    end
-  end
+# Defines the `Shrine` storage locations for different data types.
+module ShrineStorage
+  UPLOADS = "uploads"
 end
+
