7c303c83655a611d60f7e0c77bde1ef7112b500b
nolyoi <n.mayersky@hey.com>
2021-03-22 17:00:03 -0500

    Attempt 6000

--- config/shrine.cr
+++ config/shrine.cr
@@ -1,14 +1,16 @@
 Shrine.configure do |config|
   if Lucky::Env.test?
-    config.storages["cache"] = Storage::FileSystem.new("uploads", prefix: "cache")
-    config.storages["store"] = Storage::FileSystem.new("uploads")
+    config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::Memory.new
   elsif Lucky::Env.test?
-    config.storages["cache"] = Storage::FileSystem.new("uploads", prefix: "cache")
-    config.storages["store"] = Storage::FileSystem.new("uploads")
+    config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::FileSystem.new("tmp", prefix: "assets/uploads")
   else
     client = Awscr::S3::Client.new(ENV["BUCKETEER_AWS_REGION"], ENV["BUCKETEER_AWS_ACCESS_KEY_ID"], ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"])
-    storage = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"] , client: client, prefix: "public/uploads", public: true)
+    storage = Shrine::Storage::S3.new(bucket: ENV["BUCKETEER_BUCKET_NAME"] , client: client, prefix: "public", public: true)
     config.storages["cache"] = Storage::FileSystem.new("uploads", prefix: "cache")
     config.storages["store"] = storage
   end
+end
+# Defines the `Shrine` storage locations for different data types.
+module ShrineStorage
+  UPLOADS = "uploads"
 end
