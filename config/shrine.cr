10717a4bdbe913b158abf412f0d26914aaa088d5
nolyoi <n.mayersky@hey.com>
2021-03-22 15:17:54 -0500

    Potential fix for bucketeer

--- config/shrine.cr
+++ config/shrine.cr
@@ -1,7 +1,7 @@
 Shrine.configure do |config|
   if Lucky::Env.test?
     config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::Memory.new
-  elsif Lucky::Env.development?
+  elsif Lucky::Env.test?
     config.storages[ShrineStorage::UPLOADS] = Shrine::Storage::FileSystem.new("tmp", prefix: "assets/uploads")
   else
     client = Awscr::S3::Client.new(ENV["BUCKETEER_AWS_REGION"], ENV["BUCKETEER_AWS_ACCESS_KEY_ID"], ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"], endpoint: "https://bucketeer-7b46713e-bb3a-4b4f-a6f3-36fbd08c6baa.s3.amazonaws.com/public/")
